a640199f6499c3595c83373efc4c14e1b4ebd88c
UCHIYAMA Ryota <lessironglance@gmail.com>
2023-12-24 23:09:24 +0900

    update voicevox sound is playing, wait for text message

--- src/Clients/VoicevoxClient.cs
+++ src/Clients/VoicevoxClient.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Net.Http;
 using System.Media;
+using System.Threading.Channels;
 
 namespace Medoz.MessageTransporter.Clients;
 
@@ -22,6 +23,10 @@ public class VoicevoxClient: ISpeakerClient
 
     private uint _speakerId;
 
+    private Channel<string>? _messageChannel;
+
+    private CancellationTokenSource _messageCancelTokenSource = new();
+
     public VoicevoxClient(uint speakerId, string? url = null)
     {
         if (url is not null)
@@ -41,13 +46,14 @@ public class VoicevoxClient: ISpeakerClient
 
     public async Task RunAsync()
     {
+        if (!string.IsNullOrEmpty(_version))
+        {
+            throw new Exception("VOICEVOX is running");
+        }
+
         try
         {
             _version = await _httpClient.GetStringAsync(_url + _versionPath);
-            if (OnReady is not null)
-            {
-                await OnReady.Invoke();
-            }
         }
         catch
         {
@@ -56,30 +62,57 @@ public class VoicevoxClient: ISpeakerClient
 
         _heartbeat = new Heartbeat(new TimeSpan(0, 1, 0));
         _heartbeat.OnHeartbeat += OnHeartbeat;
+        _messageChannel = Channel.CreateUnbounded<string>();
+        RunChannelReaderAsync();
+
+        if (OnReady is not null)
+        {
+            await OnReady.Invoke();
+        }
     }
 
 
     public Task StopAsync()
     {
         _version = null;
+        if (_heartbeat is not null)
+        {
+            _heartbeat.OnHeartbeat -= OnHeartbeat;
+            _heartbeat = null;
+        }
+        if (_messageChannel is not null)
+        {
+            _messageCancelTokenSource.Cancel();
+            _messageChannel = null;
+        }
         return Task.CompletedTask;
     }
 
     public async Task SpeakMessageAsync(string message)
     {
-        string encodedMessage = System.Net.WebUtility.UrlEncode(message);
-        var audioQueryResponse = await _httpClient.PostAsync($"{_url}{_audioQuery}?text={encodedMessage}&speaker={_speakerId}", null);
-        if (audioQueryResponse.StatusCode != System.Net.HttpStatusCode.OK)
+        if (_messageChannel is null) throw new NullReferenceException("Channel is null");
+        await _messageChannel.Writer.WriteAsync(message);
+    }
+
+    private async Task RunChannelReaderAsync()
+    {
+        if (_messageChannel is null) throw new NullReferenceException("Channel is null");
+        await foreach(var message in _messageChannel.Reader.ReadAllAsync().WithCancellation(_messageCancelTokenSource.Token))
         {
-            throw new Exception($"audio_query response is {audioQueryResponse.StatusCode}");
+            string encodedMessage = System.Net.WebUtility.UrlEncode(message);
+            var audioQueryResponse = await _httpClient.PostAsync($"{_url}{_audioQuery}?text={encodedMessage}&speaker={_speakerId}", null);
+            if (audioQueryResponse.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception($"audio_query response is {audioQueryResponse.StatusCode}");
+            }
+
+            var voiceResponse = await _httpClient.PostAsync($"{_url}{_synthesis}?speaker={_speakerId}", audioQueryResponse.Content);
+            var content = await voiceResponse.Content.ReadAsByteArrayAsync();
+
+            using var stream = new MemoryStream(content);
+            using var player = new SoundPlayer(stream);
+            player.PlaySync();
         }
-
-        var voiceResponse = await _httpClient.PostAsync($"{_url}{_synthesis}?speaker={_speakerId}", audioQueryResponse.Content);
-        var content = await voiceResponse.Content.ReadAsByteArrayAsync();
-
-        using var stream = new MemoryStream(content);
-        using var player = new SoundPlayer(stream);
-        player.Play();
     }
 
     private async void OnHeartbeat(object? _)
