02381439843fcccb342700fe0d6ff2a3bd5a03bb
UCHIYAMA Ryota <lessironglance@gmail.com>
2023-12-29 22:26:22 +0900

    update add logging

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Threading;
+
+using Microsoft.Extensions.Logging;
 
 namespace Medoz.MessageTransporter;
 
@@ -10,10 +10,19 @@ namespace Medoz.MessageTransporter;
 /// </summary>
 public partial class App : Application
 {
-        public void HandleException(
+    
+    public void HandleException(
             object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine($"Handled: {e.Exception.Message}");
-            e.Handled = true;
-        }
+    {
+        System.Diagnostics.Debug.WriteLine($"Handled: {e.Exception.Message}");
+        var logger = LoggerUtility.GetLoggerFactory().CreateLogger("UnhandledException");
+        logger.LogError(e.Exception, "UnhandledException");
+        e.Handled = true;
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+    }
 }
