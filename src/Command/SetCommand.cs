8448ae21d81014f8cb7a3191874e9e098137c786
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-04-04 01:09:29 +0900

    SetCommandに設定プロパティを変更する機能を追加し、引数の検証とログメッセージを強化

--- src/Command/SetCommand.cs
+++ src/Command/SetCommand.cs
@@ -3,21 +3,76 @@ using System.Runtime.InteropServices;
 using System.Windows.Input;
 
 using Medoz.KoeKan.Clients;
+using Medoz.KoeKan.Services;
 using Medoz.KoeKan.Data;
 namespace Medoz.KoeKan.Command;
 
 public class SetCommand : ICommand
 {
     public string CommandName => "set";
-    public string HelpText => "set command";
+    public string HelpText => "set config property";
+
+    private readonly IListenerService _listenerService;
+    private readonly IConfigService _configService;
+
+    public SetCommand(IListenerService listenerService, IConfigService configService)
+    {
+        _listenerService = listenerService;
+        _configService = configService;
+    }
 
     public bool CanExecute(string[] args)
     {
-        return args.Length > 0 && args[0] == CommandName;
+        return args.Length > 0;
     }
 
     public async Task ExecuteCommandAsync(string[] args)
     {
+        if (!CanExecute(args))
+        {
+            _listenerService.AddLogMessage("Invalid arguments for set command.");
+            return;
+        }
+
+        var config = _configService.GetConfig();
+        switch (args[0])
+        {
+            case "username":
+                if (args.Length != 2)
+                {
+                    _listenerService.AddLogMessage("Username not specified.");
+                    return;
+                }
+                config.Username = args[1];
+                _configService.SaveConfig();
+                _listenerService.AddLogMessage($"Username set to {args[1]}.");
+                break;
+            case "icon":
+                if (args.Length != 2)
+                {
+                    _listenerService.AddLogMessage("icon not specified.");
+                    return;
+                }
+                config.Icon = args[1];
+                _configService.SaveConfig();
+                _listenerService.AddLogMessage($"Icon set to {args[1]}.");
+                break;
+            case "application":
+                if (args.Length < 2)
+                {
+                    _listenerService.AddLogMessage("application args not found.");
+                    return;
+                }
+                config.Applications.Concat(args[1..]);
+                _configService.SaveConfig();
+                _listenerService.AddLogMessage($"append application to {args[1..]}.");
+                break;
+            default:
+                // FIXME: 設定画面を開く
+                _listenerService.AddLogMessage($"Unknown set command: {args[0]}");
+                break;
+        }
+
         await Task.CompletedTask;
     }
 }
