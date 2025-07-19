4932bf1b94677096a0453e22ae004986dc80d71c
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-07-19 23:56:24 +0900

    fix: MessageクラスのTimestampプロパティをDateTimeOffset型に変更し、UTC時間を使用するように修正

--- src/Messaging/Message.cs
+++ src/Messaging/Message.cs
@@ -5,11 +5,11 @@ public record Message(
     string Channel,
     string Username,
     string Content,
-    DateTime Timestamp,
+    DateTimeOffset Timestamp,
     string? IconSource = null)
     {
         public Message(string clientType, string channel, string username, string content, string? iconSource = null)
-            : this(clientType, channel, username, content, DateTime.Now, iconSource)
+            : this(clientType, channel, username, content,  DateTimeOffset.UtcNow.DateTime, iconSource)
             {
 
             }
