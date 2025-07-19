fdf7c0602174a8b1b7753732a9a8984191a9a1d0
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-07-19 23:55:34 +0900

    refactor: IAsyncEventBusインターフェースのコメントを追加し、コードの可読性を向上

--- src/Messaging/IAsyncEventBus.cs
+++ src/Messaging/IAsyncEventBus.cs
@@ -1,11 +1,39 @@
 namespace Medoz.CatChast.Messaging;
 
+/// <summary>
+/// Provides an asynchronous event bus for publishing and subscribing to events.
+/// </summary>
 public interface IAsyncEventBus
 {
+    /// <summary>
+    /// Subscribes a synchronous handler to events of type T.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="handler"></param>
     void Subscribe<T>(Action<T> handler);
+
+    /// <summary>
+    /// Subscribes an asynchronous handler to events of type T.
+    /// </summary>
     void SubscribeAsync<T>(Func<T, Task> handler);
+
+    /// <summary>
+    /// Unsubscribes a synchronous handler from events of type T.
+    /// </summary>
     void Unsubscribe<T>(Action<T> handler);
+
+    /// <summary>
+    /// Unsubscribes an asynchronous handler from events of type T.
+    /// </summary>
     void UnsubscribeAsync<T>(Func<T, Task> handler);
+
+    /// <summary>
+    /// Publishes an event to all subscribed handlers asynchronously.
+    /// </summary>
     Task PublishAsync<T>(T eventData);
+
+    /// <summary>
+    /// Removes all subscribed handlers.
+    /// </summary>
     void Clear();
 }
